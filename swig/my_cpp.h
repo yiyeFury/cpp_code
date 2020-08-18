89248c76534339008ff25a4f5a9889fc678fad17
yiyeFury <691597456@qq.com>
2020-08-18 22:53:25 +0800

    update .gitignore

--- swig/my_cpp.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-int leap_year(int year);
-void array_cos(double * in_array, double * out_array, int size);
-
--- swig/my_cpp.i
+++
@@ -1,8 +0,0 @@
-%module my_cpp
-
-%{
-#define SWIG_FILE_WITH_INIT
-#include "my_cpp.h"
-%}
-
-int leap_year(int year);
