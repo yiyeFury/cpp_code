b6124a3616c8ea2b73e789118f40626eb3d8d308
yiyeFury <691597456@qq.com>
2020-08-21 02:02:15 +0800

    Update sort_algorithm.cpp

--- my_cpp/my_cpp/sort_algorithm.cpp
+++ my_cpp/my_cpp/sort_algorithm.cpp
@@ -3,23 +3,59 @@
 
 using namespace std;
 
-void insertion_sort(int *arr, int num, bool is_ascending)
+void bubble_sort(int *arr, int num, bool is_ascending)
 {
-	int key, ii, jj;
-	for (jj = 1; jj < num; jj++) {
-		key = arr[jj];
-		ii = jj - 1;
-		while (ii >= 0 && arr[ii] > key) {
-			arr[ii + 1] = arr[ii];
-			ii -= 1;
-		}
-		arr[ii + 1] = key;
+	int ii, jj;
+	if (is_ascending) {
+		for (ii = 0; ii < num; ii++)
+			for (jj = num - 1; jj >ii; jj--)
+				if (arr[jj] < arr[jj - 1])  // 小于则换位置
+					swap(arr[jj], arr[jj - 1]);
 	}
+	else {
+		for (ii = 0; ii < num; ii++)
+			for (jj = num - 1; jj >ii; jj--)
+				if (arr[jj] > arr[jj - 1])  // 大于则换位置
+					swap(arr[jj], arr[jj - 1]);
+	}
+		
 
 }
 
 
-// merge sort *****************************************************************
+void insertion_sort(int *arr, int num, bool is_ascending)
+{
+	// 插入排序
+	int key, ii, jj;
+	if (is_ascending) {  // 升序
+		for (jj = 1; jj < num; jj++) {
+			key = arr[jj];
+			// insert arr[jj] into the sorted sequence arr[0] ~ arr[j-1]
+			ii = jj - 1; 
+			while (ii >= 0 && arr[ii] > key) {
+				arr[ii + 1] = arr[ii];  // 位置后移一位
+				ii -= 1;
+			}
+			arr[ii + 1] = key;
+		}
+	}else {
+		for (jj = 1; jj < num; jj++) {
+			key = arr[jj];
+			// insert arr[jj] into the sorted sequence arr[0] ~ arr[j-1]
+			ii = jj - 1;
+			while (ii >= 0 && arr[ii] < key) {
+				arr[ii + 1] = arr[ii];
+				ii -= 1;
+			}
+			arr[ii + 1] = key;
+		}
+	}
+	
+
+}
+
+
+// 快速排序 *********************************************************************
 int partition(int *arr, int p, int r)
 {
 	int x, ii;
