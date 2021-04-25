bb9344744b93b6a2000f9a408e3ac980f17605a8
yiyeFury <691597456@qq.com>
2021-04-25 21:51:52 +0800

    update common

--- common.h
+++ common.h
@@ -1,14 +1,28 @@
 #pragma once
 #include<iostream>
+#include<iomanip>
+
 using namespace std;
 
 /* -----------------------------------------------------------------------------
 打印数组
 */
 template<typename T, unsigned num>
-void print_array(T(&p)[num])
+void PrintArray1D(T(&p)[num])
 {
 	for (int ii = 0; ii < num; ii++)
-		cout << ' ' << p[ii];
+		cout <<setw(6)<< p[ii];
 	cout << endl << endl;
 }
+
+template<typename T, int M, int N>
+void PrintArray2D(const T (&arr)[M][N])
+{
+    for (int ii=0;ii<M;ii++) {
+        for(int jj=0;jj<N;jj++) {
+            cout<<setw(6)<<arr[ii][jj];
+        }
+        cout<<endl;
+    }
+}
+
--- my_main.cpp
+++ my_main.cpp
@@ -7,40 +7,28 @@
 #include "sort_algorithm.h"
 #include "my_date_time.h"
 #include "my_mathmatics.h"
+#include "common.h"
 
 using namespace std;
 
 
 int main()
 {
-    const int M=2, N=5;
+    const int M=4, N=5;
     double val = 0.0;
 
-    double arr[M][N];
+    double aa[M][N];
     for (int ii=0;ii<M;ii++) {
         for (int jj=0;jj<N;jj++) {
-            arr[ii][jj] = val;
-            val ++;
+            val++;
+            aa[ii][jj] = val;
         }
     }
 
-    double cov_mat[N][N];
-    CovarianceMatrix(arr, cov_mat);
-    for (int ii=0;ii<N;ii++) {
-        for (int jj=0;jj<N;jj++) {
-            cout<<cov_mat[ii][jj]<<"           ";
-        }
-        cout<<endl;
-    }
-    cout<<endl;
+    PrintArray2D(aa);
 
-    for (int ii =0;ii<M;ii++) {
-        for (int jj=0;jj<N;jj++) {
-            cout<<arr[ii][jj]<<"            ";
-        }
-        cout<<endl;
-    }
-
+    double bb[3] = {1,2,3};
+    PrintArray1D(bb);
 
     cout<<endl;
 //	system("pause");
