cbda34d789e2cc291d1e04bb7e927e4f588535ac
yiyeFury <691597456@qq.com>
2021-05-30 21:25:20 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,31 +14,23 @@
 
 using namespace std;
 
-template<int M>
-void Test(const char (&aa)[M])
-{
-    int cnt = 0;
-    for (auto ii=begin(aa);ii!=end(aa); ii++) {
-        cnt ++;
-        cout<<setw(5)<<*ii;
-    }
-    cout<<endl<<"number is: "<<cnt<<endl;
-}
+
 
 int main()
 {
-    char a1[] = {'C', '+', '+'};  // 列表初始化，没有空字符
-    char a2[] = {'C', '+', '+', '\0'};  // 列表初始化，含有显式的空字符
-    char a3[] = "C++";  // 自动添加表示字符串结束的空字符
+    int a1[5][10];
+    int cnt = 0;
+    for (auto &row:a1) {
+        for (auto &col:row) {
+            col = ++cnt;
+        }
+    }
 
-    Test(a1);
-    Test(a2);
-    Test(a3);
-
-
-//    PrintArray(a1);
-//    PrintArray(a2);
-//    PrintArray(a3);
+    for (auto &row:a1) {
+        for (auto col:row) {
+            cout<<setw(5)<<col;
+        }
+    }
 
 
     cout<<endl;
