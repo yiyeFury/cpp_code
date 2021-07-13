60aec5b9e2564e0c56c2334eb5e902945e9a72d8
yiyeFury <691597456@qq.com>
2021-07-13 22:49:58 +0800

    update nothing

--- main.cpp
+++ main.cpp
@@ -22,6 +22,8 @@
 //
 //#include <opencv2/opencv.hpp>
 //
+
+#include <memory>
 using namespace std;
 //using namespace cv;
 
@@ -30,22 +32,28 @@ using namespace std;
 
 int main()
 {
-   cout<<"Start\n";
-   
-   int p = 10;
-   
-   using arr = int[10];  // typedef int arr[10];
-   int *pia = new arr;
-   
-   int cnt = 0;
-   for (int ii=0;ii<p;ii++)
-       *(pia+ii) = cnt++;
+    cout << "Start\n";
+    int n = 10;
+    allocator<string> alloc;
+    auto const p = alloc.allocate(n);  // 分配n个未初始化的string
     
-    for (int ii=0;ii<p;ii++)
-        cout<<setw(10)<<*(pia+ii);
-
-   cout<<"\nend\n"<<endl;
-   cout<<endl;
-	  // system("pause");
-   return 0;
+    allocator<int> alloc1;
+    allocator<int>::pointer alloc_ptr;
+    
+    alloc_ptr = alloc1.allocate(10);
+    int ii;
+    for (ii = 0; ii < 10; ii++) {
+        alloc_ptr[ii] = ii;
+    }
+    
+    for (ii = 0; ii < 10; ii++) {
+        cout << setw(5) << alloc_ptr[ii];
+    }
+    
+    alloc1.deallocate(alloc_ptr, 10);
+    
+    cout << "\nend\n" << endl;
+    cout << endl;
+    // system("pause");
+    return 0;
 }
