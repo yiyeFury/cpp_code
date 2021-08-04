29f3fa778cbf4bbf94b275fa7537b2cab5f4c6f0
yiyeFury <691597456@qq.com>
2021-08-04 22:35:14 +0800

    update eigen note

---
+++ eigen_note.cpp
@@ -0,0 +1,29 @@
+//
+// Created by xie on 2021/8/4.
+//
+
+#include <iostream>
+#include <Dense>
+
+using namespace std;
+using namespace Eigen;
+
+// int main()
+// {
+//     cout << "\nStart\n\n";
+//     int a=3, b=4;
+//     Matrix<float, Dynamic, Dynamic> A(a, b);
+//
+//     int cnt = 0;
+//     for (int ii=0;ii<a;ii++) {
+//         for (int jj=0;jj<b;jj++) {
+//             A(ii, jj) = ++cnt;
+//         }
+//     }
+//
+//     cout<<A;
+//
+//     cout << "\n\nend\n" << endl;
+//     // system("pause");
+//     return 0;
+// }
--- main.cpp
+++ main.cpp
@@ -8,29 +8,19 @@
 #include <memory>
 
 #include "common.h"
-#include <Eigen/Dense>
 
 using namespace std;
-using namespace Eigen;
 //using namespace cv;
 
 
-// #include "ML/linear_regression.h"
-// #include "ML/logistic_regression.h"
-// #include "DSA/linked_list.h"
-
-
-int main()
-{
-    cout << "Start\n";
-    
-    Matrix<float, 2, Dynamic> A;
-    
-    
-    cout<<endl<<endl;
-    
-    cout << "\nend\n" << endl;
-    cout << endl;
-    // system("pause");
-    return 0;
-}
+// int main()
+// {
+//     cout << "Start\n";
+//
+//     cout<<endl<<endl;
+//
+//     cout << "\nend\n" << endl;
+//     cout << endl;
+//     // system("pause");
+//     return 0;
+// }
