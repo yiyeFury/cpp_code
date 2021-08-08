b2d44d05106e6d11da43076c641cd13f85b87f11
yiyeFury <691597456@qq.com>
2021-08-08 22:44:07 +0800

    update linked list

--- DSA/linked_list.cpp
+++ DSA/linked_list.cpp
@@ -27,35 +27,42 @@ bool DoublyLinkedList::Full()
     return size_ == length_;
 }
 
-void DoublyLinkedList::Insert(DoublyLinkedNode *prev_node, DoublyLinkedNode *tmp_Node)
+void DoublyLinkedList::Insert(DoublyLinkedNode *prev_node, DoublyLinkedNode *tmp_node)
 {
     // 将 tmp_Node 插入到 prev_node 节点之后
-    if (Full()) {
+    if (Full()) {  // 链表已满
         return;
-    }
-    
-    
-    if (prev_node == head_) {
-        head_ = tmp_Node;
+    }else if (Empty()) {  // 链表为空
+        head_ = tmp_node;
         head_ -> prev = nullptr;
+        
+        tail_ = tmp_node;
+        tail_ -> next = nullptr;
+        
     } else {
         DoublyLinkedNode *old_next = prev_node->next;
         
         // 前向拼接
-        prev_node->next = tmp_Node;
-        tmp_Node ->prev = prev_node;
-        
+        prev_node->next = tmp_node;
+        tmp_node ->prev = prev_node;
+    
         // 后向拼接
-        tmp_Node -> next = old_next;
-        old_next -> prev = tmp_Node;
+        tmp_node -> next = old_next;
+        
+        // 当前节点，是否为 尾结点
+        if (prev_node==tail_) {
+            tail_ = tmp_node;
+        } else {
+            old_next -> prev = tmp_node;
+        }
     }
+    size_++;
 }
 
 
 void DoublyLinkedList::Delete(DoublyLinkedNode *tmp_node)
 {
     if (Empty()) {
-        
         return;
     }
     
@@ -75,6 +82,10 @@ void DoublyLinkedList::Delete(DoublyLinkedNode *tmp_node)
 DoublyLinkedNode *DoublyLinkedList::Search(int k)
 {
     // 给定 key 值，搜索 所在节点
+    if (Empty()) {
+        return nullptr;
+    }
+    
     DoublyLinkedNode *tmp_node = head_;
     while (tmp_node != tail_ && tmp_node->key != k) {
         tmp_node = tmp_node->next;
